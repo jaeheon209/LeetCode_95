b2e32c47578e97c662be2b020129e431d845b0c9
jacob-branch <jacob.branch@kakaocorp.com>
2021-10-11 22:54:20 +0900

    Add 15. 206. reverse-linked-list

---
+++ src/main/java/com/leetcode/interview/_15_206/Solution.java
@@ -0,0 +1,50 @@
+package com.leetcode.interview._15_206;
+
+import java.util.List;
+
+// 206. https://leetcode.com/problems/reverse-linked-list/
+public class Solution {
+
+    public static void main(String[] args) {
+
+        // input
+        ListNode head = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(3,
+                                new ListNode(4,
+                                        new ListNode(5)))));
+
+        // process
+        ListNode answer = reverseList(head);
+
+        //output
+        outputPrint(answer);
+
+    }
+
+
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev; //head
+
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public static void outputPrint(ListNode answer) {
+        if (answer != null) {
+            while (answer.next != null) {
+                System.out.print(answer.val + ", ");
+                answer = answer.next;
+            }
+            System.out.println(answer.val);
+        }
+    }
+
+
+}
